448ae02ef2f0b46ec7ac453dea67bd4c8475f9ca
Adi Dahari <dahariadi16@gmail.com>
2021-10-03 20:21:32 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express();
+const socketIO = require('socket.io');
+
+const port=3000;
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  var data = {
+    cards: {
+      haifa: { title: "Haifa", value: 500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "home" },
+      dan: { title: "Dan", value: 1500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "store" },
+      central: { title: "Center", value: 3500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "info_outline" },
+      south: { title: "South", value: 700, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "add_shopping_cart" }
+    }
+  }
+  res.render("pages/dashboard", data)
+})
+
+app.get('/setData/:districtId/:value', function (req, res) {
+  io.emit('newdata',{districtId:req.params.districtId,value:req.params.value})
+  
+  res.send(req.params.value)
+})
+
+
+const server = express()
+  .use(app)
+  .listen(3000, () => console.log(`Listening Socket on http://localhost:3000`));
+const io = socketIO(server);
+
+//------------
+// io.on('connection', (socket) => {  
+//   socket.on('newdata', (msg) => {
+//     console.log(msg);
+//     io.emit('newdata', msg);
+//   });
+// });
+//-----------
+
