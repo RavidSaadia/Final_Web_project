a696af36c0a2b1957744f0f4f6db4870c01df750
Adi Dahari <dahariadi16@gmail.com>
2021-10-04 11:19:54 +0300

    first commit

--- server.js
+++ server.js
@@ -10,19 +10,18 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   var data = {
-    cards: {
-      haifa: { title: "Haifa", value: 500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "home" },
-      dan: { title: "Dan", value: 1500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "store" },
-      central: { title: "Center", value: 3500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "info_outline" },
-      south: { title: "South", value: 700, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "add_shopping_cart" }
-    }
+    cards: [
+      {districtId:"haifa", title: "Haifa", value: 500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "home" },
+      {districtId:"dan", title: "Dan", value: 1500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "store" },
+      {districtId:"central", title: "Center", value: 3500, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "info_outline" },
+      {districtId:"south", title: "South", value: 700, unit: "Packages", fotterIcon: "", fotterText: "Average Size", icon: "add_shopping_cart" }
+    ]
   }
   res.render("pages/dashboard", data)
 })
 
 app.get('/setData/:districtId/:value', function (req, res) {
   io.emit('newdata',{districtId:req.params.districtId,value:req.params.value})
-  
   res.send(req.params.value)
 })
 
@@ -32,12 +31,12 @@ const server = express()
   .listen(3000, () => console.log(`Listening Socket on http://localhost:3000`));
 const io = socketIO(server);
 
-//------------
-// io.on('connection', (socket) => {  
-//   socket.on('newdata', (msg) => {
-//     console.log(msg);
-//     io.emit('newdata', msg);
-//   });
-// });
-//-----------
+
+io.on('connection', (socket) => {  
+  socket.on('newdata', (msg) => {
+    console.log(msg);
+    io.emit('newdata', msg);
+  });
+});
+
 
